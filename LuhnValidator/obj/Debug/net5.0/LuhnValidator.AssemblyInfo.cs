4911235a5133518c70efa5502385c1552e18cf11
Reynel Axel Perdomo Medina <ReynelAxel@outlook.com>
2021-03-08 20:22:15 -0400

    Fixed the package metadata

--- LuhnValidator/obj/Debug/net5.0/LuhnValidator.AssemblyInfo.cs
+++ LuhnValidator/obj/Debug/net5.0/LuhnValidator.AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AxelCreations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("AxelCreations 2021")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Credit Cards and Identification numbers Validator. Based on the Luhn Algorithm.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
