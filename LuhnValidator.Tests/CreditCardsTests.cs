d965af446610201bda2c6aaced7802d4a5a181d6
Reynel Axel Perdomo Medina <ReynelAxel@outlook.com>
2021-03-13 23:28:21 -0400

    Added Unit Testing Project

---
+++ LuhnValidator.Tests/CreditCardsTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace AxelCreations.Validators.LuhnValidator.Tests
+{
+    public class CreditCardsTests
+    {
+        [Fact]
+        public void CreditCardIsValid()
+        {
+            bool isValid = Validate.CreditCard("4000-0012-3456-7899");
+
+            Assert.True(isValid, "This credit card is valid");
+        }
+
+        [Fact]
+        public void CreditCardIsInvalid()
+        {
+            bool isValid = Validate.CreditCard("5000-0012-3456-7899");
+
+            Assert.False(isValid, "This is not a valid credit card");
+        }
+    }
+}
---
+++ LuhnValidator.Tests/PersonalIDTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace AxelCreations.Validators.LuhnValidator.Tests
+{
+    public class PersonalIDTests
+    {
+        [Fact]
+        public void PersonalIDIsValid()
+        {
+            bool isValid = Validate.PersonalID("001-1285166-2");
+
+            Assert.True(isValid, "This personal ID card is valid");
+        }
+
+        [Fact]
+        public void PersonalIDIsInvalid()
+        {
+            bool isValid = Validate.PersonalID("8581-0000000-1");
+
+            Assert.False(isValid, "This is not a valid personal ID");
+        }
+    }
+}
